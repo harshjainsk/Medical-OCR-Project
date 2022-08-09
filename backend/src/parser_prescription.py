d5092b59490fc2d367f3bc07c2e2c1525570435f
harshjainsk <harshjainsk531@gmail.com>
2022-08-09 19:05:08 +0530

    refactored the code

--- backend/src/parser_prescription.py
+++ backend/src/parser_prescription.py
@@ -9,39 +9,30 @@ class PrescriptionParser(MedicalDocParser):
 
     def parse(self):
         return {
-            'patient_name': self.get_name(),
-            'patient_address': self.get_address(),
-            'medicines': self.get_medicines(),
-            'direction': self.get_directions(),
-            'refill': self.get_refill()
+            'patient_name': self.get_field('patient_name'),
+            'patient_address': self.get_field('patient_address'),
+            'medicines': self.get_field('medicines'),
+            'direction': self.get_field('directions'),
+            'refill': self.get_field('refill')
         }
 
-    def get_name(self):
-        pattern = 'Name:(.*)Date'
-        matches = re.findall(pattern, self.text)
+    def get_field(self, field_name):
 
-        if len(matches) > 0:
-            return matches[0].strip()
+        pattern_dict = {
+            'patient_name': {'pattern': 'Name:(.*)Date', 'flags': 0},
+            'patient_address': {'pattern': 'Address:(.*)\n', 'flags': 0},
+            'medicines': {'pattern': 'Address:[^\n]*(.*)Direction', 'flags': re.DOTALL},
+            'directions': {'pattern': 'Directions:(.*)Refill', 'flags': re.DOTALL},
+            'refill': {'pattern': 'Refill: (\d+) times', 'flags': 0},
+        }
 
-    def get_address(self):
-        pattern = 'Address:(.*)\n'
-        matches = re.findall(pattern, self.text)
+        pattern_object = pattern_dict.get(field_name)
 
-        if len(matches) > 0:
-            return matches[0].strip()
+        if pattern_object:
+            matches = re.findall(pattern_object['pattern'], self.text, flags=pattern_object['flags'])
 
-    def get_medicines(self):
-        pattern = "Address:[^\n]*(.*)Direction"
-        matches = re.findall(pattern, self.text, flags=re.DOTALL)
-
-        if len(matches) > 0:
-            return matches[0].strip()
-
-    def get_directions(self):
-        pass
-
-    def get_refill(self):
-        pass
+            if len(matches) > 0:
+                return matches[0].strip()
 
 
 if __name__ == '__main__':
